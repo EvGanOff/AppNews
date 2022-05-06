aa898fa334e2145cd81aff6952d106599dfd1393
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-05-06 14:48:44 +0300

    configured NavigationCotroller and TabBar

--- AppNews/Source/Controllers/NBookmarksViewController.swift
+++ AppNews/Source/Controllers/NBookmarksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NBookmarksViewController.swift
 //  AppNews
 //
 //  Created by Евгений Ганусенко on 5/6/22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NBookmarksViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
 }
-
---
+++ AppNews/Source/CustomViews/Labels/NBodyLabel.swift
@@ -0,0 +1,34 @@
+//
+//  NBodyLabel.swift
+//  AppNews
+//
+//  Created by Евгений Ганусенко on 5/6/22.
+//
+
+import UIKit
+
+class NBodyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    convenience init(textAligment: NSTextAlignment) {
+        self.init(frame: .zero)
+        self.textAlignment = textAligment
+    }
+
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        numberOfLines = 1
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
